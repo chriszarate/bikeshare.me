cf9508b326fc3f7cc84634d1544cc2d3d9bbe17a
Chris Zarate <chris@zarate.org>
2013-06-14 00:51:14 -0400

    Custom timeago strings.

---
+++ app/js/config/timeago-config.js
@@ -0,0 +1,15 @@
+/* jQuery Timeago configuration */
+
+$.timeago.settings.refreshMillis = 5000;
+$.timeago.settings.strings = $.extend($.timeago.settings.strings, {
+  suffixAgo: "",
+  seconds: "Updated",
+  minute: "Updated",
+  minutes: "%d mins",
+  hour: "1 hr",
+  hours: "%d hrs",
+  day: "1 day",
+  days: "%d days",
+  month: "1 month",
+  months: "%d months"
+});
