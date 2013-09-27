2134406cd423ccb4f187ce332572d989290a2d17
Chris Zarate <chris@zarate.org>
2013-09-27 12:00:23 -0400

    Use single quotes.

--- app/js/config/timeago-config.js
+++ app/js/config/timeago-config.js
@@ -2,14 +2,14 @@
 
 $.timeago.settings.refreshMillis = 5000;
 $.timeago.settings.strings = $.extend($.timeago.settings.strings, {
-  suffixAgo: "",
-  seconds: "Updated",
-  minute: "1 min",
-  minutes: "%d mins",
-  hour: "1 hr",
-  hours: "%d hrs",
-  day: "1 day",
-  days: "%d days",
-  month: "1 month",
-  months: "%d months"
+  suffixAgo: '',
+  seconds: 'Updated',
+  minute: '1 min',
+  minutes: '%d mins',
+  hour: '1 hr',
+  hours: '%d hrs',
+  day: '1 day',
+  days: '%d days',
+  month: '1 month',
+  months: '%d months'
 });
