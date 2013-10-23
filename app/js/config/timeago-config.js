eb58d800f4e444816f80b0200797d8286c6b8836
chriszarate <chris@zarate.org>
2013-10-22 21:20:05 -0400

    Condense time descriptions to save horizontal space.

--- app/js/config/timeago-config.js
+++ app/js/config/timeago-config.js
@@ -4,12 +4,12 @@ $.timeago.settings.refreshMillis = 5000;
 $.timeago.settings.strings = $.extend($.timeago.settings.strings, {
   suffixAgo: '',
   seconds: 'Updated',
-  minute: '1 min',
-  minutes: '%d mins',
-  hour: '1 hr',
-  hours: '%d hrs',
-  day: '1 day',
-  days: '%d days',
-  month: '1 month',
-  months: '%d months'
+  minute: '1m',
+  minutes: '%dm',
+  hour: '1h',
+  hours: '%dh',
+  day: '1d',
+  days: '%dd',
+  month: '1mo',
+  months: '%dmo'
 });
