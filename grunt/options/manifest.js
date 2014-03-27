d3feca30668e0f1dfec596f49b6e4232448b1fe7
chriszarate <chris@zarate.org>
2014-03-26 22:49:45 -0400

    Move cache manifest back to root.

--- grunt/options/manifest.js
+++ grunt/options/manifest.js
@@ -3,11 +3,12 @@
 module.exports = {
   generate: {
     options: {
-      basePath: 'app/',
+      basePath: 'app',
       cache: [
         'http://themes.googleusercontent.com/static/fonts/lato/v6/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff',
         'http://themes.googleusercontent.com/static/fonts/lato/v6/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff'
       ],
+      process: function(path) { return '/' + path; },
       network: ['*'],
       fallback: ['/ /'],
       verbose: true,
@@ -18,6 +19,6 @@ module.exports = {
       'build/app.min.js',
       'build/app.min.css'
     ],
-    dest: 'app/build/cache.manifest'
+    dest: 'app/cache.manifest'
   }
 };
